73d9ea8a11af8e20f59676dc9abb500522cc8257
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-07-19 17:55:42 +0300

    сортивовка (разворот) связанного списка

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList<string> link = new LinkedList<string>(); // Создаем связный список
+            
+            link.AddFirst("Alena"); // Добавляем несколько элементов
+            link.AddFirst("Dima");
+            link.AddFirst("Boris");
+            link.AddFirst("Dasha");
+
+            LinkedListNode<string> node; // Выводим список в прямом направлении
+            Console.WriteLine("Наш список: ");
+            for (node = link.First; node != null; node = node.Next)
+                Console.Write(node.Value + "\t");
+
+            Console.WriteLine("\nСписок в обратном направлении: "); // Выводим список в обратном направлении
+            for (node = link.Last; node != null; node = node.Previous)
+                Console.Write(node.Value + "\t");
+
+            
+        }
+    }
+}
